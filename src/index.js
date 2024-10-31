120a10889625ad540883bdbee6965c77b9deb29c
Kevin Garcia Martin <kevingm@live.se>
2024-10-31 10:23:34 +0100

    Add rendering projects to menu

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import "./static/css/reset.css";
 import "./static/css/style.css";
 import RenderProfile from "./static/js/components/RenderProfile";
+import RenderProjectToMenu from "./static/js/components/RenderProjectToMenu";
 import { createTask } from "./static/js/components/task";
 import { createProject } from "./static/js/components/project";
 import { formatISO9075, addDays } from "date-fns";
 
-const initApp = (() => {
+const app = (() => {
   const projects = [];
   const defaultProject = createProject("Default Project");
+  const p2 = createProject("Project 2");
+  const p3 = createProject("Project 3");
 
   defaultProject.addTask(
     createTask("Task 1", "Task 1 description.", new Date(), 2)
@@ -20,6 +23,8 @@ const initApp = (() => {
   );
 
   projects.push(defaultProject);
+  projects.push(p2);
+  projects.push(p3);
 
   const tasksAsc = defaultProject.getSortedTasks("priority", "ascending");
   const tasksDesc = defaultProject.getSortedTasks("priority", "descending");
@@ -31,11 +36,14 @@ const initApp = (() => {
   console.log("");
   tasksDesc.forEach((task) => console.log(task));
   console.log("");
+
+  return { projects };
 })();
 
 const renderApp = (() => {
   RenderProfile();
 
+  app.projects.forEach((project) => RenderProjectToMenu(project));
   const menu = document.querySelector("#menu");
   const content = document.querySelector("#content");
 })();
